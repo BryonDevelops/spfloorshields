ac7f2e78f9ae0cc70a9e24a2037377655fa3c9e4
Bryon Bauer <70969195+BryonDevelops@users.noreply.github.com>
2023-05-21 15:47:46 -0500

    Remove src structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/html/*.html', "./build/js/*.js"],
+  content: ["./build/**/*.{html,js,css}"],
   theme: {
     screens: {
       'xs': {'min': '300px', 'max': '640px'},
