06193f62b5df7122e886045934b88156817aba3d
Bryon Bauer <70969195+BryonDevelops@users.noreply.github.com>
2023-05-26 19:20:09 -0500

    Minor changes for resolutions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   content: ["./**/*.{html,js,css}","./build/**/*.{html,js,css}"],
   theme: {
     screens: {
-      'xs': {'min': '300px', 'max': '640px'},
-      'sm': {'min': '641px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px'},
+      'xs': { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
+      'sm': { min: '576px', max: '897px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+      'md': { min: '898px', max: '1199px' }, // Tablet (matches max: iPad Pro @ 1112px).
+      'lg': { min: '1200px' }, // Desktop smallest.
+      'xl': { min: '1159px' }, // Desktop wide.
+      '2xl': { min: '1359px' } // Desktop widescreen.
     },
     extend: {
       colors: {
